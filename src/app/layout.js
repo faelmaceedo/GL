287e68499531147695f00fd6d2a8b0cd20428946
faelmaceedo <42850186+faelmaceedo@users.noreply.github.com>
2026-02-09 20:04:22 -0300

    setup de monitoramento (analytics + insights)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Guilherme Leão | Especialista em Direito Previdenciário',
@@ -12,7 +14,13 @@ export default function RootLayout({ children }) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css" />
       </head>
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body suppressHydrationWarning={true}>
+        {children}
+        
+        {/* 2. Injeção das ferramentas de monitoramento da Vercel */}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   )
 }
